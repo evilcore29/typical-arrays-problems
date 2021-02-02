41aeaf51c4a0da674e56e40168aee5f420472452
evilcore29 <evilcore29@gmail.com>
2021-02-02 13:15:21 +0300

    feat: implement min, max and avg calculate functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  return array.sort((a, b) => a - b)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  let counter = 0;
+  const arrLength = array.length;
+  const result =
+  array.map((number) => (counter += number))[arrLength - 1] / arrLength;
+  return parseFloat((result / 10) * 10).toFixed(2);
 }
